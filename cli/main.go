fc4488dc0979310ba0e4df6591c6d949518f4dda
artaasadi <arta-c@kaytu.io>
2023-11-27 00:47:17 +0100

    fix: cliend and server settled

--- cli/main.go
+++ cli/main.go
@@ -4,22 +4,25 @@ import (
 	"fmt"
 	"github.com/kaytu-io/pennywise/cli/parser/terraform"
 	"github.com/kaytu-io/pennywise/cli/usage"
-	"github.com/kaytu-io/pennywise/server/resource"
-	"golang.org/x/net/context"
+	"github.com/kaytu-io/pennywise/server/client"
 	"io"
 	"log"
 	"os"
 	"sort"
 )
 
+var (
+	ServerClientAddress = os.Getenv("SERVER_CLIENT_URL")
+)
+
 func main() {
-	file, err := os.Open("../../terracost_test/image/tfplan.json")
+	file, err := os.Open("../../terracost_test/linux_virtual_machine/tfplan.json")
 	if err != nil {
 		fmt.Println(err)
 		log.Fatal(err)
 	}
 	usage := usage.Default
-	err = EstimateTerraformPlan(context.Background(), file, usage)
+	err = EstimateTerraformPlan(file, usage)
 	if err != nil {
 		fmt.Println(err)
 		log.Fatal(err)
@@ -29,7 +32,7 @@ func main() {
 // EstimateTerraformPlan is a helper function that reads a Terraform plan using the provided io.Reader,
 // generates the prior and planned cost.State, and then creates a cost.Plan from them that is returned.
 // It uses the Backend to retrieve the pricing data.
-func EstimateTerraformPlan(ctx context.Context, plan io.Reader, u usage.Usage, providerInitializers ...terraform.ProviderInitializer) error {
+func EstimateTerraformPlan(plan io.Reader, u usage.Usage, providerInitializers ...terraform.ProviderInitializer) error {
 	if len(providerInitializers) == 0 {
 		providerInitializers = getDefaultProviders()
 	}
@@ -40,30 +43,19 @@ func EstimateTerraformPlan(ctx context.Context, plan io.Reader, u usage.Usage, p
 	}
 	tfplan.SetUsage(u)
 
-	priorQueries, err := tfplan.ExtractPriorQueries()
-	if err != nil {
-		return err
-	}
-	fmt.Println("PRIOR")
-	fmt.Println(priorQueries)
-	// If it's the first time we run the plan, then we might not have
-	// prior queries so we ignore it and move forward
-	//prior, err := cost.NewState(ctx, be, priorQueries)
-	//if err != nil && err != terraform.ErrNoQueries {
-	//	return nil, err
-	//}
-
 	plannedQueries, err := tfplan.ExtractPlannedQueries()
 	if err != nil {
 		return err
 	}
-	fmt.Println("PLANNED")
-	fmt.Println(plannedQueries)
-	resource.Resource{}
-	//planned, err := cost.NewState(ctx, be, plannedQueries)
-	//if err != nil {
-	//	return nil, err
-	//}
+	for _, rs := range plannedQueries {
+		res := rs.ToResource("")
+		serverClient := client.NewPennywiseServerClient(ServerClientAddress)
+		cost, err := serverClient.GetCost(res)
+		if err != nil {
+			return err
+		}
+		fmt.Println(rs.Address, ":", cost)
+	}
 
 	modules := make([]string, 0, 0)
 	for k := range tfplan.Configuration.RootModule.ModuleCalls {
