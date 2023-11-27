b2f94bd667a6774e61d9e7367088fca773ce6574
artaasadi <arta-c@kaytu.io>
2023-11-27 13:30:18 +0100

    feat: add cli

---
+++ cli/cmd/ingest.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"github.com/kaytu-io/pennywise/cli/cmd/flags"
+	"github.com/kaytu-io/pennywise/server/client"
+	"github.com/spf13/cobra"
+	"os"
+	"strings"
+)
+
+var (
+	ServerClientAddress = os.Getenv("SERVER_CLIENT_URL")
+)
+
+var ingest = &cobra.Command{
+	Use:   "ingest",
+	Short: `Store pricing data in the server database.`,
+	Long:  `Store pricing data in the server database for the specified provider and resource type and region.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		provider := flags.ReadStringFlag(cmd, "provider")
+		service := flags.ReadStringFlag(cmd, "service")
+		region := flags.ReadStringFlag(cmd, "region")
+
+		serverClient := client.NewPennywiseServerClient(ServerClientAddress)
+		if strings.ToLower(provider) == "aws" {
+			err := serverClient.IngestAws(service, region)
+			if err != nil {
+				return err
+			}
+		} else if strings.ToLower(provider) == "azure" {
+			err := serverClient.IngestAzure(service, region)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(ingest)
+	ingest.Flags().String("provider", "", "cloud provider (aws | azure)")
+	ingest.MarkFlagRequired("provider")
+	ingest.Flags().String("service", "", "service")
+	ingest.MarkFlagRequired("service")
+	ingest.Flags().String("region", "", "region")
+	ingest.MarkFlagRequired("region")
+}
--- cli/go.mod
+++ cli/go.mod
@@ -2,14 +2,20 @@ module github.com/kaytu-io/pennywise/cli
 
 go 1.21.3
 
-require github.com/kaytu-io/pennywise/server v0.0.0-20231126161531-99da738f095e
+require (
+	github.com/iancoleman/strcase v0.3.0
+	github.com/kaytu-io/pennywise/server v0.0.0-20231126161531-99da738f095e
+	github.com/spf13/cobra v1.8.0
+)
 
 require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/labstack/echo/v4 v4.11.3 // indirect
 	github.com/labstack/gommon v0.4.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.15.0 // indirect
