0a0d0c8e7c66f35ebd3579b94bf3c1b4b84898e3
artaasadi <arta-c@kaytu.io>
2023-11-29 12:53:57 +0100

    fix: snapshot and references

--- server/azurerm/terraform/snapshot.go
+++ server/azurerm/terraform/snapshot.go
@@ -1,19 +1,26 @@
 package terraform
 
 import (
+	"fmt"
 	"github.com/mitchellh/mapstructure"
 	"github.com/shopspring/decimal"
 )
 
+type SourceUri struct {
+	Valuse struct {
+		DiskSizeGb *float64 `mapstructure:"disk_size_gb"`
+	} `mapstructure:"values"`
+}
+
 type snapshotValues struct {
-	Location            string    `mapstructure:"location"`
-	DiskSizeGB          *float64  `mapstructure:"disk_size_gb"`
-	SourceUri           []float64 `mapstructure:"source_uri"`
-	DiskSizeGBSourceUri float64   `mapstructure:"disk_size_gb_source_uri"` //this disk sizeGB is inside the SourceUri
+	Location   string      `mapstructure:"location"`
+	DiskSizeGB *float64    `mapstructure:"disk_size_gb"`
+	SourceUri  []SourceUri `mapstructure:"source_uri"`
 }
 
 func decodeSnapshotValues(tfVals map[string]interface{}) (snapshotValues, error) {
 	var v snapshotValues
+	fmt.Println("tfVals", tfVals)
 	config := &mapstructure.DecoderConfig{
 		WeaklyTypedInput: true,
 		Result:           &v,
@@ -27,13 +34,19 @@ func decodeSnapshotValues(tfVals map[string]interface{}) (snapshotValues, error)
 	if err := decoder.Decode(tfVals); err != nil {
 		return v, err
 	}
+	fmt.Println(v)
 	return v, nil
 }
 
 func (p *Provider) newSnapshot(vals snapshotValues) *Image {
+	var storageGB float64
+	storageSize := snapshotStorageSize(vals)
+	if storageSize != nil {
+		storageGB = *storageSize
+	}
 	return &Image{
 		imageType: "Snapshot",
-		storageGB: decimal.NewFromFloat(*snapshotStorageSize(vals)),
+		storageGB: decimal.NewFromFloat(storageGB),
 		location:  vals.Location,
 	}
 }
@@ -43,9 +56,9 @@ func snapshotStorageSize(vals snapshotValues) *float64 {
 		return vals.DiskSizeGB
 	}
 
-	if len(vals.SourceUri) > 0 {
-		size := vals.DiskSizeGBSourceUri
-		return &size
+	if vals.SourceUri != nil {
+		size := vals.SourceUri[0].Valuse.DiskSizeGb
+		return size
 	}
 
 	return nil
