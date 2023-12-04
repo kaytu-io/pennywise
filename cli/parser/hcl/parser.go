408c0492ff52914e1ee659497eea95e8fad62e96
artaasadi <arta-c@kaytu.io>
2023-12-04 16:03:14 +0100

    fix: add default usage

--- cli/parser/hcl/parser.go
+++ cli/parser/hcl/parser.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/kaytu-io/infracost/external/config"
 	"github.com/kaytu-io/infracost/external/providers/terraform"
+	"github.com/kaytu-io/pennywise/cli/usage"
 	"golang.org/x/net/context"
 )
 
@@ -47,7 +48,22 @@ func ParseHclResources(path string) (string, []Resource, error) {
 			resources[i] = makeResource.setRefs(resources, res)
 			resources[i] = makeResource.runFunctions(res)
 		}
+		resources[i] = addUsage(res)
+		//fmt.Println("RES WITH USAGE", resources[i])
 	}
 
 	return provider, resources, nil
+}
+
+func addUsage(res Resource) Resource {
+	newValues := res.Values
+	//fmt.Println("TYPE", res.Type)
+	newValues[usage.Key] = usage.Default.GetUsage(res.Type)
+	return Resource{
+		Address: res.Address,
+		Mode:    res.Mode,
+		Name:    res.Name,
+		Type:    res.Type,
+		Values:  newValues,
+	}
 }
