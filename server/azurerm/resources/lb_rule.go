aef17a39ca5a1b6e98e0e482af4f0facaa939a58
artaasadi <arta-c@kaytu.io>
2023-12-08 10:28:30 +0100

    fix: load balancer rule

--- server/azurerm/resources/lb_rule.go
+++ server/azurerm/resources/lb_rule.go
@@ -4,10 +4,80 @@ import (
 	"github.com/kaytu-io/pennywise/server/internal/product"
 	"github.com/kaytu-io/pennywise/server/internal/query"
 	"github.com/kaytu-io/pennywise/server/internal/util"
+	"github.com/mitchellh/mapstructure"
 	"github.com/shopspring/decimal"
 )
 
-func (inst *LoadBalancer) regionalIncludedRulesComponent(key, location string) query.Component {
+type SourceLoadBalancer struct {
+	Values struct {
+		Sku      string `mapstructure:"sku"`
+		Location string `mapstructure:"location"`
+		SkuTier  string `mapstructure:"sku_tier"`
+	} `mapstructure:"values"`
+}
+
+type LoadBalancerRule struct {
+	provider *Provider
+
+	loadBalancer SourceLoadBalancer
+}
+
+type loadBalancerRuleValues struct {
+	LoadBalancer SourceLoadBalancer `mapstructure:"loadbalancer_id"`
+}
+
+// decodeLoadBalancerValues decodes and returns computeInstanceValues from a Terraform values map.
+func decodeLoadBalancerRuleValues(tfVals map[string]interface{}) (loadBalancerRuleValues, error) {
+	var v loadBalancerRuleValues
+	config := &mapstructure.DecoderConfig{
+		WeaklyTypedInput: true,
+		Result:           &v,
+	}
+
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return v, err
+	}
+
+	if err := decoder.Decode(tfVals); err != nil {
+		return v, err
+	}
+	return v, nil
+}
+
+func (p *Provider) newLoadBalancerRule(vals loadBalancerRuleValues) *LoadBalancerRule {
+	lbRule := &LoadBalancerRule{
+		provider: p,
+
+		loadBalancer: SourceLoadBalancer{
+			Values: struct {
+				Sku      string `mapstructure:"sku"`
+				Location string `mapstructure:"location"`
+				SkuTier  string `mapstructure:"sku_tier"`
+			}{
+				Sku:      "Basic",
+				Location: convertRegion(vals.LoadBalancer.Values.Location),
+				SkuTier:  "Regional",
+			},
+		},
+	}
+	if vals.LoadBalancer.Values.Sku != "" {
+		lbRule.loadBalancer.Values.Sku = vals.LoadBalancer.Values.Sku
+	}
+	if vals.LoadBalancer.Values.SkuTier != "" {
+		lbRule.loadBalancer.Values.SkuTier = vals.LoadBalancer.Values.SkuTier
+	}
+	return lbRule
+}
+
+func (inst LoadBalancerRule) Components() []query.Component {
+	if inst.loadBalancer.Values.Sku == "Basic" {
+		return nil
+	}
+	return []query.Component{RegionalOverageRulesComponent(inst.provider.key, inst.loadBalancer.Values.Location, decimal.NewFromInt(1))}
+}
+
+func RegionalIncludedRulesComponent(key, location string) query.Component {
 	return query.Component{
 		Name:            "Regional Included Rules",
 		MonthlyQuantity: decimal.NewFromInt(1),
@@ -23,7 +93,7 @@ func (inst *LoadBalancer) regionalIncludedRulesComponent(key, location string) q
 	}
 }
 
-func (inst *LoadBalancer) globalIncludedRulesComponent(key, location string) query.Component {
+func GlobalIncludedRulesComponent(key, location string) query.Component {
 	return query.Component{
 		Name:            "Global Included Rules",
 		MonthlyQuantity: decimal.NewFromInt(1),
@@ -39,7 +109,7 @@ func (inst *LoadBalancer) globalIncludedRulesComponent(key, location string) que
 	}
 }
 
-func (inst *LoadBalancer) regionalOverageRulesComponent(key, location string, overageRules decimal.Decimal) query.Component {
+func RegionalOverageRulesComponent(key, location string, overageRules decimal.Decimal) query.Component {
 	return query.Component{
 		Name:           "Regional Overage Rules",
 		HourlyQuantity: overageRules,
@@ -55,7 +125,7 @@ func (inst *LoadBalancer) regionalOverageRulesComponent(key, location string, ov
 	}
 }
 
-func (inst *LoadBalancer) globalOverageRulesComponent(key, location string, overageRules decimal.Decimal) query.Component {
+func GlobalOverageRulesComponent(key, location string, overageRules decimal.Decimal) query.Component {
 	return query.Component{
 		Name:           "Global Overage Rules",
 		HourlyQuantity: overageRules,
