2e1cd04015a081f65f48b0a8775ca69eba5794a4
artaasadi <arta-c@kaytu.io>
2024-01-14 15:27:55 +0100

    feat: use context instead of blocksMap

--- parser/my-hcl/blocks.go
+++ parser/my-hcl/blocks.go
@@ -5,7 +5,7 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/zclconf/go-cty/cty"
-	"github.com/zclconf/go-cty/cty/convert"
+	"go.uber.org/zap"
 	"strings"
 )
 
@@ -15,6 +15,10 @@ type Block struct {
 	Body        hcl.Body
 	ChildBlocks []Block
 	Attributes  []Attribute
+	Context     *hcl.EvalContext
+	CtxVariable cty.Value
+
+	logger *zap.Logger
 }
 
 var (
@@ -60,24 +64,24 @@ var (
 	invalidFunctionArgumentDiagnostic = "Invalid function argument"
 )
 
-func getFileBlocks(file *hcl.File) ([]Block, error) {
+func getFileBlocks(logger *zap.Logger, context *hcl.EvalContext, file *hcl.File) ([]Block, error) {
 	contents, _, diags := file.Body.PartialContent(terraformSchema)
 	if diags.HasErrors() {
 		return nil, diags
 	}
-	myBlocks, err := makeBlocks(&contents.Blocks, nil)
+	myBlocks, err := makeBlocks(logger, context, &contents.Blocks, nil)
 	if err != nil {
 		return nil, err
 	}
 	return myBlocks, nil
 }
 
-func makeBlocks(blocks *hcl.Blocks, childBlocks *hclsyntax.Blocks) ([]Block, error) {
+func makeBlocks(logger *zap.Logger, context *hcl.EvalContext, blocks *hcl.Blocks, childBlocks *hclsyntax.Blocks) ([]Block, error) {
 	var totalBlocks []Block
 	if blocks != nil {
 		for _, b := range *blocks {
 			if body, ok := b.Body.(*hclsyntax.Body); ok {
-				childBlocks, err := makeBlocks(nil, &body.Blocks)
+				childBlocks, err := makeBlocks(logger, context, nil, &body.Blocks)
 				if err != nil {
 					return nil, err
 				}
@@ -90,9 +94,10 @@ func makeBlocks(blocks *hcl.Blocks, childBlocks *hclsyntax.Blocks) ([]Block, err
 					Labels:      b.Labels,
 					Body:        b.Body,
 					ChildBlocks: childBlocks,
-					Attributes:  BuildAttributes(attributes),
+					Context:     context,
+					logger:      logger,
 				}
-				err = newBlock.checkForEach()
+				newBlock.buildAttributes(attributes)
 				if err != nil {
 					return nil, err
 				}
@@ -101,7 +106,7 @@ func makeBlocks(blocks *hcl.Blocks, childBlocks *hclsyntax.Blocks) ([]Block, err
 		}
 	} else if childBlocks != nil {
 		for _, b := range *childBlocks {
-			childBlocks, err := makeBlocks(nil, &b.Body.Blocks)
+			childBlocks, err := makeBlocks(logger, context, nil, &b.Body.Blocks)
 			if err != nil {
 				return nil, err
 			}
@@ -114,9 +119,10 @@ func makeBlocks(blocks *hcl.Blocks, childBlocks *hclsyntax.Blocks) ([]Block, err
 				Labels:      b.Labels,
 				Body:        b.Body,
 				ChildBlocks: childBlocks,
-				Attributes:  BuildAttributes(attributes),
+				Context:     context,
+				logger:      logger,
 			}
-			err = newBlock.checkForEach()
+			newBlock.buildAttributes(attributes)
 			if err != nil {
 				return nil, err
 			}
@@ -127,12 +133,19 @@ func makeBlocks(blocks *hcl.Blocks, childBlocks *hclsyntax.Blocks) ([]Block, err
 	return totalBlocks, nil
 }
 
-func (b Block) MakeMapStructure(mappedBlocks map[string]interface{}) (map[string]interface{}, error) {
+func (b *Block) makeMapStructure(mappedBlocks map[string]interface{}) (map[string]interface{}, error) {
 	mapStructure := make(map[string]interface{})
+	ctxMapStructure := make(map[string]cty.Value)
 	for _, attr := range b.Attributes {
-		val, err := attr.Value(mappedBlocks)
+		if attr.Name == "for_each" {
+			continue
+		}
+		val, err := attr.Value()
+		if attr.CtxVariable != nil {
+			ctxMapStructure[attr.Name] = *attr.CtxVariable
+		}
 		if err != nil {
-			return nil, err
+			continue
 		}
 		switch val.(type) {
 		case int64, int32, int:
@@ -142,9 +155,11 @@ func (b Block) MakeMapStructure(mappedBlocks map[string]interface{}) (map[string
 		case bool:
 			mapStructure[attr.Name] = val.(bool)
 		case Block:
-			blockValues, err := val.(Block).MakeMapStructure(mappedBlocks)
+			attrBlock := val.(Block)
+			blockValues, err := attrBlock.makeMapStructure(mappedBlocks)
 			if err != nil {
-				return nil, err
+				//b.logger.Error(fmt.Sprintf("error while getting %s value in block %s : %s", attr.Name, blockName, err.Error()))
+				continue
 			}
 			mapStructure[attr.Name] = blockValues
 		case []string:
@@ -154,26 +169,34 @@ func (b Block) MakeMapStructure(mappedBlocks map[string]interface{}) (map[string
 		case []int64, []int32, []int:
 			mapStructure[attr.Name] = val.([]int64)
 		default:
-			return nil, nil
+			//b.logger.Debug(fmt.Sprintf("could not find attribute %s type in block %s", attr.Name, blockName))
 		}
 	}
 	for _, childBlock := range b.ChildBlocks {
-		var blockName string
+		var childBlockName string
 		if len(childBlock.Labels) > 0 {
-			blockName = fmt.Sprintf("%s.%s", childBlock.Type, strings.Join(childBlock.Labels, "."))
+			childBlockName = fmt.Sprintf("%s.%s", childBlock.Type, strings.Join(childBlock.Labels, "."))
 		} else {
-			blockName = childBlock.Type
+			childBlockName = childBlock.Type
 		}
-		mappedChildBlock, err := childBlock.MakeMapStructure(mappedBlocks)
+		mappedChildBlock, err := childBlock.makeMapStructure(mappedBlocks)
 		if err != nil {
-			return nil, err
+			//b.logger.Error(fmt.Sprintf("error while making %s child block map structure in block %s : %s", childBlockName, blockName, err.Error()))
+			continue
 		}
-		mapStructure[blockName] = mappedChildBlock
+		ctxMapStructure[childBlock.Type] = childBlock.CtxVariable
+		mapStructure[childBlockName] = mappedChildBlock
+	}
+	ctxMapStructure["id"] = cty.EmptyObjectVal
+	b.CtxVariable = cty.ObjectVal(ctxMapStructure)
+	err := b.checkForEach()
+	if err != nil {
+		//b.logger.Error(fmt.Sprintf("error while parsing for each on block %s : %s", blockName, err.Error()))
 	}
 	return mapStructure, nil
 }
 
-func (b Block) findAttribute(name string) *Attribute {
+func (b *Block) findAttribute(name string) *Attribute {
 	for _, attr := range b.Attributes {
 		if attr.Name == name {
 			return &attr
@@ -182,142 +205,16 @@ func (b Block) findAttribute(name string) *Attribute {
 	return nil
 }
 
-func (b Block) checkForEach() error {
+func (b *Block) checkForEach() error {
 	forEach := b.findAttribute("for_each")
 	if forEach == nil {
 		return nil
 	}
-	ctx := hcl.EvalContext{}
-	ctx.Variables = make(map[string]cty.Value)
-	//ctx.Variables["local"] = cty.ObjectVal(map[string]cty.Value{"permutations": cty.ObjectVal(make(map[string]cty.Value))})
-	//fmt.Println(ctx.Variables)
 
-	for _, traversal := range forEach.HclAttribute.Expr.Variables() {
-		var rootName string
-		for _, traverser := range traversal {
-			fmt.Println("Traversal", traversal)
-			if r, ok := traverser.(hcl.TraverseRoot); ok {
-				rootName = r.Name
-				break
-			}
-		}
-
-		ob := ctx.Variables[rootName]
-		fmt.Println("pb", ob)
-		if ob.IsNull() || !ob.IsKnown() {
-			ob = cty.ObjectVal(make(map[string]cty.Value))
-		}
-
-		ctx.Variables[rootName] = buildObject(traversal, ob, 0)
-		fmt.Println(ctx.Variables)
-	}
-	ctyVal, diag := forEach.HclAttribute.Expr.Value(&ctx)
+	ctyVal, diag := forEach.HclAttribute.Expr.Value(b.Context)
 	if diag.HasErrors() {
-		for _, d := range diag {
-			fmt.Println("diag", d.Detail)
-			if d.Summary == missingAttributeDiagnostic || d.Summary == valueDoesNotHaveAnyIndices {
-				fmt.Println(missingAttributeDiagnostic, valueDoesNotHaveAnyIndices)
-			}
-			if d.Summary == valueIsNonIterableDiagnostic {
-				fmt.Println(valueIsNonIterableDiagnostic)
-			}
-			if d.Summary == invalidFunctionArgumentDiagnostic {
-				fmt.Println(invalidFunctionArgumentDiagnostic)
-			}
-		}
-		return diag
+		return nil
 	}
-	fmt.Println("ctyVal", ctyVal)
-	ctyVal.ForEachElement(func(key cty.Value, val cty.Value) bool {
-		fmt.Println("here2")
-		fmt.Println("key:", key)
-		fmt.Println("val:", val)
-		return false
-	})
+	fmt.Println("foreach ctyVal", ctyVal.AsValueMap())
 	return nil
-}
-
-func buildObject(traversal hcl.Traversal, value cty.Value, i int) cty.Value {
-	if i > len(traversal)-1 {
-		return value
-	}
-	traverser := traversal[i]
-
-	var valueMap map[string]cty.Value
-
-	if value.IsKnown() && !value.IsNull() && value.CanIterateElements() {
-		valueMap = value.AsValueMap()
-	}
-
-	if valueMap == nil {
-		valueMap = make(map[string]cty.Value)
-	}
-
-	// traverse splat is a special holding type which means we want to traverse all the attributes on the map.
-	if _, ok := traverser.(hcl.TraverseSplat); ok {
-		for k, v := range valueMap {
-			if v.Type().IsObjectType() {
-				valueMap[k] = buildObject(traversal, v, i+1)
-				continue
-			}
-
-			valueMap[k] = v
-		}
-
-		return cty.ObjectVal(valueMap)
-	}
-
-	if index, ok := traverser.(hcl.TraverseIndex); ok {
-		kc, err := convert.Convert(index.Key, cty.String)
-		if err != nil {
-			kc = cty.StringVal("0")
-		}
-
-		k := kc.AsString()
-
-		if vv, exists := valueMap[k]; exists {
-			valueMap[k] = buildObject(traversal, vv, i+1)
-			return cty.ObjectVal(valueMap)
-		}
-
-		valueMap[k] = buildObject(traversal, cty.ObjectVal(make(map[string]cty.Value)), i+1)
-
-		return cty.ObjectVal(valueMap)
-	}
-
-	if v, ok := traverser.(hcl.TraverseAttr); ok {
-		fmt.Println("TRAVERSER NAME", v.Name)
-		if len(traversal)-1 == i {
-			// if the attribute already exists, and we're not setting a list value
-			// then we should return here. It's most likely that we weren't able to
-			// get the full variable calls for the context, so resetting the value could
-			// be harmful.
-			if _, exists := valueMap[v.Name]; exists {
-				return value
-			}
-			valueMap[v.Name] = cty.ObjectVal(make(map[string]cty.Value))
-			return cty.ObjectVal(valueMap)
-		}
-		if vv, exists := valueMap[v.Name]; exists {
-			if isList(vv) {
-				items := make([]cty.Value, vv.LengthInt())
-				it := vv.ElementIterator()
-				for it.Next() {
-					key, sourceItem := it.Element()
-					val := buildObject(traversal, sourceItem, i+1)
-					i, _ := key.AsBigFloat().Int64()
-					items[i] = val
-				}
-				valueMap[v.Name] = cty.TupleVal(items)
-				return cty.ObjectVal(valueMap)
-			}
-
-			valueMap[v.Name] = buildObject(traversal, vv, i+1)
-			return cty.ObjectVal(valueMap)
-		}
-
-		valueMap[v.Name] = buildObject(traversal, cty.ObjectVal(make(map[string]cty.Value)), i+1)
-		return cty.ObjectVal(valueMap)
-	}
-	return buildObject(traversal, value, i+1)
 }
