032a570fc0113362bbe4960ba1eb68108f605626
artaasadi <arta-c@kaytu.io>
2024-01-21 11:45:15 +0100

    fix: multiple locals

--- parser/hcl/context.go
+++ parser/hcl/context.go
@@ -25,22 +25,38 @@ func makeCtxVariables(ctxVariableMap map[string]interface{}) map[string]cty.Valu
 func updateBlockCtxVariableMap(ctxVariableMap map[string]interface{}, b Block, additionalLabels ...string) map[string]interface{} {
 	blockType := GetBlockTypeByType(b.Type)
 	if blockType.name == "resource" {
-		ctxVariableMap = updateCtxVariableMapByLabels(ctxVariableMap, b, append(b.Labels, additionalLabels...))
+		ctxVariableMap = updateCtxVariableMapByLabels(ctxVariableMap, b.CtxVariable, append(b.Labels, additionalLabels...))
 	} else {
 		if len(b.Labels)+len(additionalLabels) > 0 {
 			if _, ok := ctxVariableMap[blockType.refName]; !ok {
 				ctxVariableMap[blockType.refName] = make(map[string]interface{})
 			}
-			ctxVariableMap[blockType.refName] = updateCtxVariableMapByLabels(ctxVariableMap[blockType.refName].(map[string]interface{}), b, append(b.Labels, additionalLabels...))
+			ctxVariableMap[blockType.refName] = updateCtxVariableMapByLabels(ctxVariableMap[blockType.refName].(map[string]interface{}), b.CtxVariable, append(b.Labels, additionalLabels...))
 		} else {
-			ctxVariableMap[blockType.refName] = b.CtxVariable
+			if _, ok := ctxVariableMap[blockType.refName]; ok {
+				if _, ok := ctxVariableMap[blockType.refName].(map[string]interface{}); ok {
+					valueMap := b.CtxVariable.AsValueMap()
+					for k, v := range valueMap {
+						ctxVariableMap[blockType.refName] = updateCtxVariableMapByLabels(ctxVariableMap[blockType.refName].(map[string]interface{}), v, []string{k})
+					}
+				}
+			} else {
+				if b.CtxVariable.AsValueMap() != nil {
+					ctxVariableMap[blockType.refName] = make(map[string]interface{})
+					for k, v := range b.CtxVariable.AsValueMap() {
+						ctxVariableMap[blockType.refName] = updateCtxVariableMapByLabels(ctxVariableMap[blockType.refName].(map[string]interface{}), v, []string{k})
+					}
+				} else {
+					ctxVariableMap[blockType.refName] = b.CtxVariable
+				}
+			}
 		}
 	}
 	return ctxVariableMap
 }
 
 // updateCtxVariableMapByLabels updates context variable map by getting a new block and its labels
-func updateCtxVariableMapByLabels(ctxVariableMap map[string]interface{}, b Block, labels []string) map[string]interface{} {
+func updateCtxVariableMapByLabels(ctxVariableMap map[string]interface{}, variable cty.Value, labels []string) map[string]interface{} {
 	key := labels[0]
 	if len(labels) > 1 {
 		if _, ok := ctxVariableMap[key]; !ok {
@@ -48,9 +64,18 @@ func updateCtxVariableMapByLabels(ctxVariableMap map[string]interface{}, b Block
 		} else if _, ok := ctxVariableMap[key].(map[string]interface{}); !ok {
 			ctxVariableMap[key] = make(map[string]interface{})
 		}
-		ctxVariableMap[key] = updateCtxVariableMapByLabels(ctxVariableMap[key].(map[string]interface{}), b, labels[1:])
+		ctxVariableMap[key] = updateCtxVariableMapByLabels(ctxVariableMap[key].(map[string]interface{}), variable, labels[1:])
 	} else {
-		ctxVariableMap[key] = b.CtxVariable
+		if _, ok := ctxVariableMap[key]; ok {
+			if _, ok := ctxVariableMap[key].(map[string]interface{}); ok {
+				valueMap := variable.AsValueMap()
+				for k, v := range valueMap {
+					ctxVariableMap[key] = updateCtxVariableMapByLabels(ctxVariableMap[key].(map[string]interface{}), v, []string{k})
+				}
+			}
+		} else {
+			ctxVariableMap[key] = variable
+		}
 	}
 	return ctxVariableMap
 }
