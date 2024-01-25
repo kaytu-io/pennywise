8ae2b73d61bc5b0ea5d5049d7e8b0c4d873900f4
artaasadi <arta-c@kaytu.io>
2024-01-25 11:17:56 +0100

    feat: add wait to get function

--- cmd/ingestion/get.go
+++ cmd/ingestion/get.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/kaytu-io/pennywise/cmd/flags"
+	"github.com/kaytu-io/pennywise/pkg/schema"
 	"github.com/kaytu-io/pennywise/pkg/server"
 	"github.com/spf13/cobra"
+	"time"
 )
 
 var get = &cobra.Command{
@@ -14,18 +16,32 @@ var get = &cobra.Command{
 	Long:  `Returns an ingestion job with the provided id`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		id := flags.ReadStringFlag(cmd, "id")
+		wait := flags.ReadBooleanFlag(cmd, "wait")
 
 		serverClient := server.NewPennywiseServerClient(flags.ReadStringFlag(cmd, "server-url"))
 		job, err := serverClient.GetIngestionJob(id)
 		if err != nil {
 			return err
 		}
-
+		if wait && !(job.Status == schema.IngestionJobSucceeded || job.Status == schema.IngestionJobFailed) {
+			fmt.Println(fmt.Sprintf("Waiting for job %d to be completed...", job.ID))
+			var maxWaiting int
+			for maxWaiting < 3600/10 {
+				job, err = serverClient.GetIngestionJob(fmt.Sprintf("%d", job.ID))
+				if err != nil {
+					return err
+				}
+				if job.Status == schema.IngestionJobSucceeded || job.Status == schema.IngestionJobFailed {
+					fmt.Println("Job is finished:", job.Status)
+					return nil
+				}
+				time.Sleep(time.Second * 10)
+			}
+		}
 		jobJSON, err := json.MarshalIndent(*job, "", "    ")
 		if err != nil {
 			return err
 		}
-
 		fmt.Println(string(jobJSON))
 		return nil
 	},
