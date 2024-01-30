02af09236a90731744c2369b071a62c2e66c9bb1
artaasadi <arta-c@kaytu.io>
2024-01-30 10:26:16 +0100

    feat: output costs with go-pretty

--- pkg/cost/state.go
+++ pkg/cost/state.go
@@ -2,6 +2,9 @@ package cost
 
 import (
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/kaytu-io/pennywise/pkg/schema"
 )
 
@@ -17,6 +20,18 @@ var (
 	ErrProductNotFound = fmt.Errorf("product not found")
 	ErrPriceNotFound   = fmt.Errorf("price not found")
 )
+
+var primary = color.New(color.FgHiCyan)
+
+var yellow = color.New(color.FgYellow)
+var red = color.New(color.FgHiRed)
+var green = color.New(color.FgHiGreen)
+
+var bold = color.New(color.Bold)
+var faint = color.New(color.Faint)
+var underline = color.New(color.Underline)
+
+var primaryLink = color.New(color.Underline).Add(color.Bold)
 
 // Cost returns the sum of the costs of every Resource included in this State.
 // Error is returned if there is a mismatch in resource currencies.
@@ -51,34 +66,106 @@ func (s *State) GetCostComponents() []Component {
 // CostString returns a string to show the breakdown of the costs for a state
 // containing the resources and their components costs and total cost for the resources and the state
 func (s *State) CostString() (string, error) {
+	var costString string
+
+	t := table.NewWriter()
+	t.Style().Options.DrawBorder = false
+	t.Style().Options.SeparateColumns = false
+	t.Style().Options.SeparateRows = false
+	t.Style().Options.SeparateHeader = false
+	t.Style().Format.Header = text.FormatDefault
+
+	var columns []table.ColumnConfig
+	i := 1
+	var headers table.Row
+	headers = append(headers, underline.Sprint("Name"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+
+	headers = append(headers, underline.Sprint("Unit Price"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+	headers = append(headers, underline.Sprint("Hourly Qty"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+	headers = append(headers, underline.Sprint("Monthly Qty"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignLeft,
+		AlignHeader: text.AlignLeft,
+	})
+	i++
+
+	headers = append(headers, underline.Sprint("Unit"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignRight,
+		AlignHeader: text.AlignRight,
+	})
+	i++
+
+	headers = append(headers, underline.Sprint("Monthly Cost"))
+	columns = append(columns, table.ColumnConfig{
+		Number:      i,
+		Align:       text.AlignRight,
+		AlignHeader: text.AlignRight,
+	})
+	i++
+
+	t.AppendRow(table.Row{""})
+
+	t.SetColumnConfigs(columns)
+	t.AppendHeader(headers)
+
 	var unsupportedServices []string
 	cost, err := s.Cost()
 	if err != nil {
 		return "", err
 	}
-	costString := fmt.Sprintf("- Total Cost (per month): %v", cost.Decimal.Round(3))
+
 	for name, rs := range s.Resources {
 		if !rs.IsSupported {
 			unsupportedServices = append(unsupportedServices, rs.Type)
 			continue
 		}
-
-		rsCostString, err := rs.CostString()
+		cost, err := rs.Cost()
 		if err != nil {
 			return "", err
 		}
-		costString = fmt.Sprintf("%s\n--- Costs for %s :", costString, name)
-		costString = fmt.Sprintf("%s\n%s", costString, rsCostString)
+		var row table.Row
+		row = append(row, bold.Sprint(name), "", "", "", "", cost.Decimal.Round(2))
+		costRows, err := rs.CostRows()
+		if err != nil {
+			return "", err
+		}
+		t.AppendRow(row)
+		for _, r := range costRows {
+			t.AppendRow(r)
+		}
 	}
-
+	costString = t.Render()
+	costString += "\n──────────────────────────────────\n"
+	costString += fmt.Sprintf("%s:    %v", bold.Sprint("Total Cost (per month)"), cost.Decimal.Round(2))
 	if len(unsupportedServices) == 3 {
-		costString = fmt.Sprintf("%s\n---- Resource types %s, %s and %s not supported", costString, unsupportedServices[0], unsupportedServices[1], unsupportedServices[2])
+		costString = fmt.Sprintf("%s\n- Resource types %s, %s and %s not supported", costString, unsupportedServices[0], unsupportedServices[1], unsupportedServices[2])
 	} else if len(unsupportedServices) == 2 {
-		costString = fmt.Sprintf("%s\n---- Resource types %s and %s not supported", costString, unsupportedServices[0], unsupportedServices[1])
+		costString = fmt.Sprintf("%s\n- Resource types %s and %s not supported", costString, unsupportedServices[0], unsupportedServices[1])
 	} else if len(unsupportedServices) == 1 {
-		costString = fmt.Sprintf("%s\n---- Resource type %s not supported", costString, unsupportedServices[0])
+		costString = fmt.Sprintf("%s\n- Resource type %s not supported", costString, unsupportedServices[0])
 	} else if len(unsupportedServices) > 3 {
-		costString = fmt.Sprintf("%s\n---- Resource types %s, %s, %s and %d other Resource types not supported", costString, unsupportedServices[0], unsupportedServices[1], unsupportedServices[2], len(unsupportedServices)-3)
+		costString = fmt.Sprintf("%s\n- Resource types %s, %s, %s and %d other Resource types not supported", costString, unsupportedServices[0], unsupportedServices[1], unsupportedServices[2], len(unsupportedServices)-3)
 	}
 
 	return costString, nil
